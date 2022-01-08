6295d05b4d8680047ad4cb164fb990a932331daf
Gowtham G M <gowtham.muralivel@mallow-tech.com>
2022-01-08 14:25:16 +0530

    commit the changes

--- app/src/main/java/com/gmg/android/toastmessageare/MainActivity.kt
+++ app/src/main/java/com/gmg/android/toastmessageare/MainActivity.kt
@@ -2,10 +2,12 @@ package com.gmg.android.toastmessageare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gmg.android.toastalpha.GToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GToast(context = this).toastMessage()
     }
 }
